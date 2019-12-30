14f23d1b2768385618afdc0518b904d370c2a6fe
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2019-12-30 22:17:20 +0100

    Style changes and finished styling

--- script.js
+++ script.js
@@ -97,23 +97,68 @@ let heroes = [
 ];
 
 const heroesElem = document.querySelector('.heroes');
+const paginationElem = document.querySelector('.page-numbers');
 
-heroes.forEach(hero => {
-    getHeroesInformation(hero);
-});
+let currentPage = 1;
+let cards = 4;
 
-function getHeroesInformation(hero) {
-    console.log(hero);
+function displayList(heroes, container, cardsPerPage, page) {
+    container.innerHTML = '';
+    page--;
 
-    let heroElem = document.createElement('div');
+    let start = cardsPerPage * page;
+    let end = start + cardsPerPage;
 
-    heroElem.classList.add('hero');
+    let paginatedItems = heroes.slice(start, end);
 
-    heroElem.innerHTML = `
+    paginatedItems.forEach(hero => {
+
+        let heroElem = document.createElement('div');
+        heroElem.classList.add('hero');
+
+        heroElem.innerHTML = `
             <h2>${hero.name}</h2>
             <img src="${hero.img}" />
             <p>${hero.bio}</p>
-    `;
+        `;
 
-    heroesElem.appendChild(heroElem);
+        container.appendChild(heroElem);
+    });
 }
+
+function setupPagination(heroes, container, cardsPerPage) {
+    container.innerHTML = '';
+
+    let pageCount = Math.ceil(heroes.length / cardsPerPage);
+    
+    for (let i = 1; i < pageCount + 1; i++) {
+        let btn = paginationBtn(i, heroes);
+
+        container.appendChild(btn);
+    }
+}
+
+function paginationBtn(page, heroes) {
+    let btn = document.createElement('button');
+    btn.innerText = page;
+
+    if (currentPage == page) {
+        btn.classList.add('active');
+    }
+
+    btn.addEventListener('click', function() {
+        currentPage = page;
+
+        displayList(heroes, heroElem, cards, currentPage);
+
+        let currentBtn = document.querySelector('.page-numbers button.active');
+        currentBtn.classList.remove('active');
+
+        btn.classList.add('active');
+    });
+
+    return btn;
+}
+
+displayList(heroes, heroesElem, cards, currentPage);
+setupPagination(heroes, paginationElem, cards);
